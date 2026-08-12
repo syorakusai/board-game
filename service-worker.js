db7111881ec394a90011d732f75ec3b0163f4e63
syorakusai <hironepeace@gmail.com>
2026-08-12 23:58:01 +0900

    先読み札の終了処理とリバース演出を修正

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kizoku-no-hisomegoto-v10";
+const CACHE_NAME = "kizoku-no-hisomegoto-v11";
 const APP_SHELL = [
   "./",
   "index.html",
@@ -44,4 +44,3 @@ self.addEventListener("fetch", event => {
     return response;
   })));
 });
-
